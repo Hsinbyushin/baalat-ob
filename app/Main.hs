80c234d64a940f6bf29003cff5b0d5e1dac40334
Tristan Hinkel <tristan_hinkel@web.de>
2026-08-08 10:06:55 +0200

    feat: add readable dry-run output

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
 module Main where
 
+import BaalatOb.Person
+    ( Person(..)
+    )
+
 import BaalatOb.Wikidata
     ( fetchDeathAnniversaries
     )
@@ -9,9 +13,14 @@ import Data.Time
     , utctDay
     )
 
+import Control.Monad (forM_)
+
+
 main :: IO ()
 main = do
     putStrLn "Ba'alat 'Ob"
+    putStrLn "Dry run"
+    putStrLn ""
 
     now <- getCurrentTime
 
@@ -19,11 +28,43 @@ main = do
             utctDay now
 
     putStrLn $
-        "Fetching death anniversaries for "
+        "Date: "
             ++ show today
-            ++ "..."
+
+    putStrLn ""
+    putStrLn "Fetching memorial candidates from Wikidata..."
+    putStrLn ""
 
     people <-
         fetchDeathAnniversaries today
 
-    print people
+    printSummary people
+
+
+-- Prints a human-readable dry-run summary.
+printSummary :: [Person] -> IO ()
+printSummary people = do
+    putStrLn $
+        "Found "
+            ++ show (length people)
+            ++ " memorial candidate(s)."
+
+    putStrLn ""
+
+    forM_ people printPerson
+
+
+-- Prints one memorial candidate in a readable format.
+printPerson :: Person -> IO ()
+printPerson person = do
+    putStrLn (name person)
+
+    putStrLn $
+        "  Died: "
+            ++ show (dateOfDeath person)
+
+    putStrLn $
+        "  X: @"
+            ++ xUsername person
+
+    putStrLn ""
